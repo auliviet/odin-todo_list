2e17dbc0cc4ab2dbb2b2386c6c102fc21e7c4f73
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-07-20 17:57:33 +1000

    Add function to manage due dates of new tasks

--- src/tasks.js
+++ src/tasks.js
@@ -1,11 +1,26 @@
 export class Task {
     constructor(obj) {
             this.title = obj.title != null ? obj.title : null;
-            this.dueDate = obj.dueDate != null ? obj.dueDate : new Date();
+            this.dueDate = this.#setDate(obj.date); 
             this.description = obj.description != null ? obj.description : null;
             this.priority = obj.priority != null ? obj.priority : null;
             this.project = obj.project != null ? obj.project : null;
             this.isComplete = obj.isComplete != null ? obj.isComplete : false;
+
+            // Remove the timestamp from dates
+            console.log(this.dueDate.getTime());
+    }
+
+    #setDate(date = null) {
+        if (date == null) {
+            date = new Date();
+        }
+
+        let year = date.getFullYear();
+        let month = date.getMonth();
+        let day = date .getDate();
+
+        return new Date(year, month, day);
     }
 
     complete() {
@@ -22,5 +37,15 @@ export class Tasks {
             let currentTask = data[task];
             this.tasks.push(new Task(currentTask));
         }
+    }
+
+    sort() {
+        // Sort tasks by priority
+
+    }
+
+    filterByDate(startDate, endDate) {
+        // Filter tasks by date
+
     }
 }
