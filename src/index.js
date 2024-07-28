76fcc5ae67eebe175b7ac602fbdb5366f0e1e6f5
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-07-28 16:59:40 +1000

    Add filters to the list of tasks

--- src/index.js
+++ src/index.js
@@ -6,4 +6,9 @@ import { Tasks } from "./tasks.js";
 
 let tasks = new Tasks(data);
 console.log(tasks);
-console.log("Overdue:", tasks.overdue);
+
+console.log("Overdue: ", tasks.overdue);
+console.log("Today: ", tasks.today);
+console.log("This week: ", tasks.thisWeek);
+console.log("This month: ", tasks.thisMonth);
+console.log("Later: ", tasks.later);
