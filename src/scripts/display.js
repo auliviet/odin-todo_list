aa79371468c0786fcc0590bad9bc72651093dcc8
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-07-29 20:47:04 +1000

    Update the DOM class to render on the page

--- src/scripts/display.js
+++ src/scripts/display.js
@@ -2,8 +2,16 @@ import { Navigation } from "../components/nav";
 import { Board } from "../components/board";
 
 export class DOM {
-    constructor() {
-        this.board = new Board();
-        console.log("DOM");
+    constructor(data) {
+        this.tasks = data;
+        console.log(this.tasks);
+
+        this.#render();   
+    }
+
+    #render() {
+        let body = document.querySelector("body");
+
+        body.append(new Board(this.tasks));
     }
 }
