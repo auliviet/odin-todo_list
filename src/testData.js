f510fa9b42553831021f8f30d181ceecd82bd3e6
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-08-17 19:46:36 +1000

    Update test data

--- src/testData.js
+++ src/testData.js
@@ -2,23 +2,40 @@ import { DateOnly } from "./scripts/tasks";
 
 export let data = [
     {
-        title: "Hello! I am a todo task",
+        title: "This task has already been completed!",
+        dueDate: DateOnly.newTaskOverdue,
+        priority: 3,
+        isComplete: true
+    },
+    {
+        title: "Hello! This is a toodoo task",
         dueDate: DateOnly.newTaskOverdue,
         priority: 1
     },
     {
-        title: "I can be edited. Just click on me!",
+        title: "To create a new task, just click on \"Add new\"",
         dueDate: DateOnly.newTaskToday,
         priority: 0
     },
     {
-        title: "I can also have a description",
-        description: "You can add more information if you need.",
+        title: "To edit an existing task, click on it.",
+        dueDate: DateOnly.newTaskToday,
+        priority: 2
+    },
+    {
+        title: "If needed, tasks can have a description too.",
+        description: "It is helpful if you need to add more informations",
         dueDate: DateOnly.newTaskThisWeek,
         priority: 3
     },
     {
-        title: "I am automatically sorted based on my due date.",
+        title: "Tasks are automatically sorted based on their due date and priority.",
+        description: "Try changing the due date of this task to see it in action.",
+        priority: 2,
+        dueDate: DateOnly.newTaskThisMonth
+    },
+    {
+        title: "You can click \"Show complete\" to see the tasks that have been completed already.",
         priority: 2,
         dueDate: DateOnly.newTaskLater
     }
